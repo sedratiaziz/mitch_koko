d97ec8ada83ee345f6aa6447d9db749e899f6345
sedratiaziz <azizsedrati72@gmail.com>
2026-05-13 14:10:25 +0300

    widgets - GRIDVIEW & GESUTREDETECTOR

--- lib/main.dart
+++ lib/main.dart
@@ -14,41 +14,57 @@ class MyApp extends StatefulWidget {
 class _MainState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+
+    List names = ['john', 'ali', 'soufiane', 'hamad'];
+
+    void printName(String name) {
+      print(name);
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            SizedBox(
-              height: 200,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Container(
-                    width: 400,
-                    color: const Color.fromARGB(255, 255, 36, 7),
-                    child: Text('Box'),
-                  ),
-                  Container(
-                    width: 400,
-                    color: Colors.amber,
-                    child: Text('Box'),
-                  ),
-                  Container(
-                    width: 400,
-                    color: const Color.fromARGB(255, 61, 255, 7),
-                    child: Text('Box'),
-                  ),
-                ],
+        
+        // *** ListView.builder() ***
+        body: SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: names.length,
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () => printName(names[index]),
+              child: Container(
+                height: 150,
+                width: 200,
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(15)),
+                child: Center(child: Text(names[index])),
               ),
             ),
-            Column(
-              children: [
-                Text('data')
-              ],
-            ),
-          ],
-        ),
+          ),
+        )
+        
+
+
+
+
+
+        
+
+        // *** GridView.builder() ***
+
+      //   body: GridView.builder(
+      //     itemCount: names.length,
+      //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8, childAspectRatio: 600/600),
+      //     itemBuilder: (context, index) => Container(
+      //       margin: EdgeInsets.all(3),
+      //       decoration: BoxDecoration(color: Colors.purpleAccent, borderRadius: BorderRadius.circular(15)),
+      //       child: Center(child: Text(names[index])),
+      //     ),
+      //   ),
+
+
+
       ),
     );
   }
