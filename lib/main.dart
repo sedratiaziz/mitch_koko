0ab2abdf913d33af68ee4466c51ac6300d7ea09c
sedratiaziz <azizsedrati72@gmail.com>
2026-06-04 16:45:31 +0300

    widgets - TEXTFIELD

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:mitch_koko/pages/explore_page.dart';
-import 'package:mitch_koko/pages/first_page.dart';
-import 'package:mitch_koko/pages/home_page.dart';
-import 'package:mitch_koko/pages/settings_page.dart';
+import 'pages/textfield_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MainState createState() => _MainState();
-}
-
-class _MainState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {
-        '/home': (context) => HomePage(),
-        '/explore': (context) => ExplorePage(),
-        '/settings': (context) => SettingsPage(),
-      },
+      home: TextfieldPage(),
     );
   }
 }
