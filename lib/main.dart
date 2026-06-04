12a05023729261d083fa79ea6d28fe795fc30acb
sedratiaziz <azizsedrati72@gmail.com>
2026-06-04 16:49:00 +0300

    projects - TODO APP initial code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'pages/textfield_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TextfieldPage(),
+      
     );
   }
 }
